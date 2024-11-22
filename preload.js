077536431c697bdec6d3e5387e1aa13765db7e8b
Edivan Batista de Lima <58676981+azzkov@users.noreply.github.com>
2024-11-22 11:01:25 -0300

    Rebuild app
    Error corrections.

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  saveXLSX: (data, fileName) => ipcRenderer.invoke('save-xlsx', data, fileName),
-  loadXLSX: () => ipcRenderer.invoke('load-xlsx')
-});
